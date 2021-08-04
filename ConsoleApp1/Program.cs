56db58caa1d2b70b4b63e9d6e1383fc212db898b
Виктор Костырин <vik.kostyrin@mail.ru>
2021-08-04 08:12:22 +0300

    Test 1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,30 +5,22 @@ namespace ConsoleApp1
 {
 	class Program
 	{
-        static double GetDiscriminant(double a, double b, double c)
-        {
-            return b * b - 4 * a * c;
-        }
-        static double GetFirstRoot(double a, double b, double c)
-        {
-            var discriminant = GetDiscriminant(a, b, c);
-            var squareroot = Math.Sqrt(discriminant);
-            return(-b - squareroot) / (2*a);
-        }
-        static double GetSecondRoot(double a, double b, double c)
-        {
-            return b / GetFirstRoot(a,b,c);
-        }
+       
         public static void Main()
 		{
-
-            double[] result =  { GetFirstRoot(3, -14, 16), GetSecondRoot(3, -14, 16) };
-            foreach (var res in result)
-            {
-                Console.WriteLine(res);
-            }
-            Console.ReadKey();
+            string userInput = Console.ReadLine();
+            Console.WriteLine(Calculate(userInput));
+            
 		}
         
+        public static double Calculate(string userInput)
+        {
+            string[] data = userInput.Split();
+            var startSum = Convert.ToDouble(data[0]);
+            var procent = Convert.ToDouble(data[1]);
+            var mounth = Convert.ToDouble(data[2]);
+            var result = startSum * Math.Pow(1 + (procent / 12) / 100, mounth);
+            return result;
+        }
     }
 }
