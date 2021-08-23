5043d82ba73bb607464e4170a644eb788e34102d
Виктор Костырин <vik.kostyrin@mail.ru>
2021-08-23 12:11:03 +0300

    Added SentencesParserTask Class

---
+++ AnalisText/FrequencyAnalysisTask.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+
+namespace TextAnalysis
+{
+    static class FrequencyAnalysisTask
+    {
+        public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
+        
+        {
+            var result = new Dictionary<string, string>();
+
+            var stat = new Dictionary<string, Dictionary<string, int>>();
+            foreach (var sens in text)
+            {
+               
+                for (int i = 0; i < sens.Count - 1; i++)
+                {
+                   
+                    if (!stat.ContainsKey(sens[i]))
+                    {
+                        var a = new Dictionary<string, int>();
+                        if (a.TryGetValue(sens[i+1], out var sometihng))
+                        {
+                            a[sens[i + 1]]++;
+                        }
+                        else
+                        {
+                            a.Add(sens[i + 1], 1);
+                        }
+                        stat.Add(sens[i], a);
+                    }
+                    else
+                    {
+                        if(stat.TryGetValue(sens[i], out var b))
+                        {
+                            if(b.TryGetValue(sens[i + 1], out var sometihng))
+                            {
+                                b[sens[i + 1]]++;
+                            }
+                            else
+                            {
+                                b.Add(sens[i + 1], 1);
+                            }
+                        }
+                    }
+                }
+                
+            }
+            foreach(var dictionaryOfWord in stat)
+            {
+                string oneGrammWord = "";
+                int maxCountofGrrammWord = 0;
+                foreach(var dictionatyOfwordOfMainWord in dictionaryOfWord.Value)
+                {
+                    if (dictionatyOfwordOfMainWord.Value > maxCountofGrrammWord)
+                    {
+                        oneGrammWord = dictionatyOfwordOfMainWord.Key;
+                        maxCountofGrrammWord = dictionatyOfwordOfMainWord.Value;
+                    }
+                    else if (dictionatyOfwordOfMainWord.Value == maxCountofGrrammWord)
+                    {
+                        if (string.CompareOrdinal(dictionatyOfwordOfMainWord.Key, oneGrammWord) < string.CompareOrdinal(oneGrammWord, dictionatyOfwordOfMainWord.Key))
+                        {
+                            oneGrammWord = dictionatyOfwordOfMainWord.Key;
+                        }
+                    }
+                }
+                if (!result.ContainsKey(dictionaryOfWord.Key))
+                {
+                    result.Add(dictionaryOfWord.Key, oneGrammWord);
+                }
+                else
+                {
+                    
+                    string slovo = "";
+                    int max  = 0;
+                    foreach(var a in stat[dictionaryOfWord.Key])
+                    {
+                        if (a.Value > max)
+                        {
+                            max = a.Value;
+                            slovo = a.Key;
+                        }
+                        else if(a.Value == max)
+                        {
+                            if (string.CompareOrdinal(a.Key, slovo) < string.CompareOrdinal(slovo, a.Key))
+                            {
+                                slovo = a.Key;
+                            }
+                        }
+                    }
+                    result[dictionaryOfWord.Key] = slovo;
+                }
+            }
+            
+            
+            return result;
+        }
+   }
+}
---
+++ AnalisText/SentencesParserTask.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Text;
+namespace TextAnalysis
+{
+    static class SentencesParserTask
+    {
+        public static List<List<string>> ParseSentences(string text)
+        {
+            var sentencesList = new List<List<string>>();
+            char[] separatorsOdSentens = new char[] {'.','!','?',':',';','(',')' };
+            string[] sentens = text.Split(separatorsOdSentens, StringSplitOptions.RemoveEmptyEntries);
+            foreach(var pred in sentens)
+            {
+                var preSentencesList = new List<string>();
+                var word = new StringBuilder();
+                for(int i = 0; i < pred.Length; i++)
+                {
+                    if (char.IsLetter(pred[i]) || pred[i] == '\'')
+                    {
+                        word.Append(pred[i]);
+                    }
+                    if (!char.IsLetter(pred[i]) && pred[i] != '\'' || i == pred.Length - 1 )
+                    {
+                        if (word.Length != 0)
+                        {
+                            preSentencesList.Add(word.ToString().ToLower());
+                            word.Clear();
+                        }
+                    }
+                }
+                if(preSentencesList.Count != 0) sentencesList.Add(preSentencesList);
+            }
+            return sentencesList;
+        }
+    }
+}
